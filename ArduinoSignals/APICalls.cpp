50f93e82b0d6eae9719a6bd612d15ea06067e4b8
Vanessa <Vanessa.Radlinger@gmail.com>
2020-04-21 18:03:12 -0500

    I made a change

--- ArduinoSignals/APICalls.cpp
+++ ArduinoSignals/APICalls.cpp
@@ -25,6 +25,8 @@ and is delayed. To see all exchange delays and terms of use, please see our disc
 
 #include "APICalls.h"
 
+// here's a change
+
 // #define PRINT_PAYLOAD
 
 String urlencode(String str)
